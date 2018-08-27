682b3ea1a0d2379e3ffa09e5540cb07d61b7c755
Tom Postler <tom@postler.me>
2018-08-26 23:37:33 -0500

    version slash command

--- src/a-slack-bot/EntryFunctions.cs
+++ src/a-slack-bot/EntryFunctions.cs
@@ -93,6 +93,23 @@ namespace a_slack_bot
             return req.CreateResponse(HttpStatusCode.OK, new { response_type = "ephemeral", text = $"{slashData.command} {slashData.text}" });
         }
 
+        [FunctionName(nameof(ReceiveSlashVersion))]
+        public static async Task<HttpResponseMessage> ReceiveSlashVersion(
+           [HttpTrigger(AuthorizationLevel.Function, "post", Route = "receive/slash/version")]HttpRequestMessage req,
+           ILogger logger)
+        {
+            if (Settings.Debug)
+                // Trim off the beginning because of AI trying to "help"
+                logger.LogInformation("Body: {0}", (await req.Content.ReadAsStringAsync()).Substring(10));
+
+            // Make sure it's a legit request
+            if (!await req.IsAuthed(logger))
+                return req.CreateErrorResponse(HttpStatusCode.Unauthorized, "Did not match hash.");
+
+            // Return the version
+            return req.CreateResponse(HttpStatusCode.OK, new { response_type = "in_channel", text = $"```{typeof(EntryFunctions).Assembly.ManifestModule.Name} v{GitVersionInformation.SemVer}```" });
+        }
+
         [FunctionName(nameof(Ping))]
         public static HttpResponseMessage Ping([HttpTrigger(AuthorizationLevel.Function, "get", Route = "ping")]HttpRequestMessage req)
         {
