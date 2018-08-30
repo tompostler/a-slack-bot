45748d2f4e412f28dfd6f7b420dd72ee2a60eb97
Tom Postler <tom@postler.me>
2018-08-29 22:04:53 -0500

    add the beginnings of sending as user

---
+++ src/a-slack-bot/Documents/OAuthToken.cs
@@ -0,0 +1,10 @@
+﻿namespace a_slack_bot.Documents
+{
+    public class OAuthToken : BaseDocument<string>
+    {
+        public override string Type => nameof(OAuthToken);
+        public override string Subtype => this.token_type;
+
+        public string token_type { get; set; }
+    }
+}
--- src/a-slack-bot/Functions/SBSlash.cs
+++ src/a-slack-bot/Functions/SBSlash.cs
@@ -15,16 +15,35 @@ namespace a_slack_bot.Functions
         [FunctionName(nameof(SBReceiveSlash))]
         public static async Task SBReceiveSlash(
             [ServiceBusTrigger(C.SBQ.InputSlash)]Messages.ServiceBusInputSlash slashMessage,
-            [DocumentDB(C.CDB.DN, C.CDB.CN, ConnectionStringSetting = C.CDB.CSS, PartitionKey = C.CDB.P, CreateIfNotExists = true)]IAsyncCollector<Documents.Event> documentCollector,
+            [DocumentDB(C.CDB.DN, C.CDB.CN, ConnectionStringSetting = C.CDB.CSS, Id = "{slashMessage.slashData.user_id}")]Documents.OAuthToken userOauthToken,
+            [DocumentDB(C.CDB.DN, C.CDB.CN, ConnectionStringSetting = C.CDB.CSS, PartitionKey = C.CDB.P, CreateIfNotExists = true)]IAsyncCollector<Documents.OAuthToken> documentCollector,
             ILogger logger)
         {
             // SB is faster than returning the ephemeral response, so just chill for a bit
             await Task.Delay(TimeSpan.FromSeconds(0.5));
 
+            // We need to decide if we should post as the user or as ourselves
+            if (userOauthToken == null)
+                await documentCollector.AddAsync(new Documents.OAuthToken { token_type = "user", Id = slashMessage.slashData.user_id });
+            var userToken = userOauthToken?.Content;
+
             var slashData = slashMessage.slashData;
 
             switch (slashData.command)
             {
+                case "/asb-send-as-me":
+                    // TODO: https://api.slack.com/docs/oauth
+                    await SendResponse(logger, slashData, ":construction: under construction :construction:", in_channel: false);
+                    break;
+
+                case "/disapprove":
+                    await SendResponse(logger, slashData, "ಠ_ಠ");
+                    break;
+
+                case "/flip":
+                    await SendResponse(logger, slashData, slashData.text + " (╯°□°)╯︵ ┻━┻");
+                    break;
+
                 case "/spaces":
                     var text = slashData.text;
                     StringBuilder sb = new StringBuilder(text.Length * 2);
@@ -34,16 +53,8 @@ namespace a_slack_bot.Functions
                     await SendResponse(logger, slashData, sb.ToString());
                     break;
 
-                case "/flip":
-                    await SendResponse(logger, slashData, slashData.text + " (╯°□°)╯︵ ┻━┻");
-                    break;
-
-                case "/disapprove":
-                    await SendResponse(logger, slashData, "ಠ_ಠ");
-                    break;
-
                 default:
-                    await SendResponse(logger, slashData, "NOT SUPPORTED");
+                    await SendResponse(logger, slashData, "*NOT SUPPORTED*", in_channel: false);
                     break;
             }
         }
