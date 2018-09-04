40318a8a461406f8022008612ce39894446e0347
Tom Postler <tom@postler.me>
2018-09-04 17:03:48 -0500

    move slash version to own file

---
+++ src/a-slack-bot/Functions/SlackEntry.SlashVersion.cs
@@ -0,0 +1,29 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace a_slack_bot.Functions
+{
+    public static partial class SlackEntry
+    {
+        [FunctionName(nameof(ReceiveSlashVersion))]
+        public static async Task<HttpResponseMessage> ReceiveSlashVersion(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "receive/slash/version")]HttpRequestMessage req,
+            ILogger logger)
+        {
+            if (Settings.Debug)
+                // Trim off the beginning because of AI trying to "help"
+                logger.LogInformation("Body: {0}", (await req.Content.ReadAsStringAsync()).Substring(10));
+
+            // Make sure it's a legit request
+            if (!await req.IsAuthed(logger))
+                return req.CreateErrorResponse(HttpStatusCode.Unauthorized, "Did not match hash.");
+
+            // Return the version
+            return req.CreateResponse(HttpStatusCode.OK, new { response_type = "in_channel", text = $"```{typeof(SlackEntry).Assembly.ManifestModule.Name} v{GitVersionInformation.SemVer}```" });
+        }
+    }
+}
--- src/a-slack-bot/Functions/SlackEntry.cs
+++ src/a-slack-bot/Functions/SlackEntry.cs
@@ -76,23 +76,6 @@ namespace a_slack_bot.Functions
             return req.CreateResponse(HttpStatusCode.OK);
         }
 
-        [FunctionName(nameof(ReceiveSlashVersion))]
-        public static async Task<HttpResponseMessage> ReceiveSlashVersion(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "receive/slash/version")]HttpRequestMessage req,
-            ILogger logger)
-        {
-            if (Settings.Debug)
-                // Trim off the beginning because of AI trying to "help"
-                logger.LogInformation("Body: {0}", (await req.Content.ReadAsStringAsync()).Substring(10));
-
-            // Make sure it's a legit request
-            if (!await req.IsAuthed(logger))
-                return req.CreateErrorResponse(HttpStatusCode.Unauthorized, "Did not match hash.");
-
-            // Return the version
-            return req.CreateResponse(HttpStatusCode.OK, new { response_type = "in_channel", text = $"```{typeof(SlackEntry).Assembly.ManifestModule.Name} v{GitVersionInformation.SemVer}```" });
-        }
-
         [FunctionName(nameof(Ping))]
         public static HttpResponseMessage Ping([HttpTrigger(AuthorizationLevel.Function, "get", Route = "ping")]HttpRequestMessage req)
         {
