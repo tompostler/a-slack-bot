a1b73d962e79f1a0705cde98ef0c4f58569ed8ab
Tom Postler <tom@postler.me>
2018-10-08 16:03:00 -0500

    data upgrade script

--- src/a-slack-bot/Documents/Event.cs
+++ src/a-slack-bot/Documents/Event.cs
@@ -2,7 +2,7 @@
 {
     public class Event : BaseDocument<Slack.Events.Inner.IEvent>
     {
-        public override string Id { get => this.Content.event_ts; set { } }
+        public override string Id { get => this.Content.event_ts ; set { } }
         public override string Type => nameof(Event);
         public override string Subtype { get => this.Content.type; set { } }
     }
---
+++ src/upgrade.181008/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace upgrade._181008
+{
+    public static class C
+    {
+        public static class CDB
+        {
+            public const string DN = "aslackbot";
+            public const string CN = "aslackbot";
+        }
+    }
+}
