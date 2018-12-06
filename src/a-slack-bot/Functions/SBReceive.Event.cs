aca6fafddd97d41217f1f07e2925f71d125b4819
Tom Postler <tom@postler.me>
2018-12-06 17:45:14 -0600

    wrong collection, right idea

--- src/a-slack-bot/Functions/SBReceive.Event.cs
+++ src/a-slack-bot/Functions/SBReceive.Event.cs
@@ -12,7 +12,7 @@ namespace a_slack_bot.Functions
         public static async Task SBReceiveEvent(
             [ServiceBusTrigger(C.SBQ.InputEvent)]Messages.ServiceBusInputEvent eventMessage,
             [DocumentDB(ConnectionStringSetting = C.CDB.CSS)]DocumentClient docClient,
-            [DocumentDB(C.CDB.DN, C.CDB.CN, ConnectionStringSetting = C.CDB.CSS, PartitionKey = C.CDB.P, CreateIfNotExists = true)]IAsyncCollector<Documents2.Event> documentCollector,
+            [DocumentDB(C.CDB2.DN, C.CDB2.Col.SlackEvents, ConnectionStringSetting = C.CDB2.CSS)]IAsyncCollector<Documents2.Event> documentCollector,
             [ServiceBus(C.SBQ.SendMessage)]IAsyncCollector<Slack.Events.Inner.message> messageCollector,
             [ServiceBus(C.SBQ.InputThread)]IAsyncCollector<Slack.Events.Inner.message> messageThreadCollector,
             ILogger logger)
