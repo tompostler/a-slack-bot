dbd745579a4adca517f05802f85a36f93a36e268
Tom Postler <tom@postler.me>
2020-06-15 21:38:51 -0500

    add release failure ping +semver:minor

--- src/a-slack-bot/Functions/HttpEntry.cs
+++ src/a-slack-bot/Functions/HttpEntry.cs
@@ -39,5 +39,34 @@ namespace a_slack_bot.Functions
             return req.CreateResponse(HttpStatusCode.OK);
         }
 
+        [FunctionName(nameof(NotifyReleaseFailure))]
+        public static async Task<HttpResponseMessage> NotifyReleaseFailure(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "internal/notify/releasefailure")] HttpRequestMessage req,
+            [ServiceBus(C.SBQ.SendMessage)] IAsyncCollector<Slack.Events.Inner.message> messageCollector,
+            ILogger logger)
+        {
+            if (Settings.Debug)
+                logger.LogInformation("Body: {0}", await req.Content.ReadAsStringAsync());
+
+            if (string.IsNullOrWhiteSpace(Settings.Runtime.NotifyChannelId))
+            {
+                logger.LogInformation("No notification channel set.");
+                return req.CreateResponse(HttpStatusCode.OK);
+            }
+
+            // Get stuff from the message
+            var desc = await req.Content.ReadAsStringAsync();
+
+            // Send it off to be processed
+            logger.LogInformation("Sending inner event into the queue.");
+            await messageCollector.AddAsync(new Slack.Events.Inner.message
+            {
+                channel = Settings.Runtime.NotifyChannelId,
+                text = $"*Failed* to release:\n```{desc}```"
+            });
+
+            // Return all is well
+            return req.CreateResponse(HttpStatusCode.OK);
+        }
     }
 }
