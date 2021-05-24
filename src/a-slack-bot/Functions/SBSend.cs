172a8663885496b6df970cafa8ca6d6d79f17e0b
Tom Postler <tom@postler.me>
2021-05-24 15:57:08 -0500

    try fixing message posting

--- src/a-slack-bot/Functions/SBSend.cs
+++ src/a-slack-bot/Functions/SBSend.cs
@@ -66,17 +66,40 @@ namespace a_slack_bot.Functions
         // This is pulled into a separate message to ease message threading scenarios.
         public static async Task<Slack.WebAPIResponse> SendMessage(Slack.Events.Inner.message message, ILogger logger)
         {
-            // Rewrite anything point to blob storage to use the cdn instead
-            message.text = message.text?.Replace(Settings.BlobsSourceHostname, Settings.BlobsTargetHostname);
+            // Convert to the bare contract for sending the message
+            var sendableMessage = new
+            {
+                message.channel,
+                message.as_user,
+                message.attachments,
+                message.blocks,
+                message.reply_broadcast,
+                // Rewrite anything point to blob storage to use the cdn instead
+                text = message.text?.Replace(Settings.BlobsSourceHostname, Settings.BlobsTargetHostname),
+                message.thread_ts,
+                message.ts,
+                message.user
+            };
 
             HttpResponseMessage response;
-            if (string.IsNullOrWhiteSpace(message.ts))
-                response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.postMessage", message);
+            if (string.IsNullOrWhiteSpace(sendableMessage.ts))
+                response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.postMessage", sendableMessage);
             else
             {
-                logger.LogInformation("Updating message {0}", message.ts);
-                message.as_user = true;
-                response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.update", message);
+                logger.LogInformation("Updating message {0}", sendableMessage.ts);
+                sendableMessage = new
+                {
+                    sendableMessage.channel,
+                    as_user = true,
+                    sendableMessage.attachments,
+                    sendableMessage.blocks,
+                    sendableMessage.reply_broadcast,
+                    sendableMessage.text,
+                    sendableMessage.thread_ts,
+                    sendableMessage.ts,
+                    sendableMessage.user
+                };
+                response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.update", sendableMessage);
             }
             logger.LogInformation("{0}: {1}", response.StatusCode, await response.Content.ReadAsStringAsync());
 
@@ -102,22 +125,33 @@ namespace a_slack_bot.Functions
         [FunctionName(nameof(SBSendMessageEphemeral))]
         public static async Task SBSendMessageEphemeral(
             [ServiceBusTrigger(C.SBQ.SendMessageEphemeral)]Slack.Events.Inner.message message,
-            [ServiceBus(C.SBQ.SendMessage)]IAsyncCollector<Slack.Events.Inner.message> messageCollector,
+            [ServiceBus(C.SBQ.SendMessage)]IAsyncCollector<object> messageCollector,
             ILogger logger)
         {
             await SR.Init(logger);
-            if (SR.C.IdToConversation.ContainsKey(message.channel) && (SR.C.IdToConversation[message.channel].is_mpim || SR.C.IdToConversation[message.channel].is_im))
+
+            // Convert to the bare contract for sending the message
+            var sendableMessage = new
+            {
+                message.channel,
+                message.as_user,
+                message.attachments,
+                message.blocks,
+                message.reply_broadcast,
+                // Rewrite anything point to blob storage to use the cdn instead
+                text = message.text?.Replace(Settings.BlobsSourceHostname, Settings.BlobsTargetHostname),
+                message.thread_ts,
+                message.ts
+            };
+
+            if (SR.C.IdToConversation.ContainsKey(sendableMessage.channel) && (SR.C.IdToConversation[sendableMessage.channel].is_mpim || SR.C.IdToConversation[sendableMessage.channel].is_im))
             {
                 logger.LogInformation("Message is in a private channel. Posting as regular message instead.");
-                message.user = null;
-                await messageCollector.AddAsync(message);
+                await messageCollector.AddAsync(sendableMessage);
                 return;
             }
 
-            // Rewrite anything point to blob storage to use the cdn instead
-            message.text = message.text?.Replace(Settings.BlobsSourceHostname, Settings.BlobsTargetHostname);
-
-            var response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.postEphemeral", message);
+            var response = await httpClient.PostAsJsonAsync("https://slack.com/api/chat.postEphemeral", sendableMessage);
             logger.LogInformation("{0}: {1}", response.StatusCode, await response.Content.ReadAsStringAsync());
 
             var responseContent = await response.Content.ReadAsAsync<Slack.WebAPIResponse>();
